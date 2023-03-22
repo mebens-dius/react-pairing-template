0a5843e4ffcff1ad1f926dc5958907a8ca0d1e5a
Michael Ebens <michael@ebens.me>
2023-03-22 11:34:49 +1000

    Added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const ExampleStyledDiv = styled.div`
+  font-family: sans-serif;
+  font-size: 1.5rem;
+  color: #333;
+  margin: 0 auto;
+  span {
+    font-weight: 900;
+  }
+`;
 
 export default function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <ExampleStyledDiv>
+        <span>Hello</span> world!
+      </ExampleStyledDiv>
     </div>
   );
 }
